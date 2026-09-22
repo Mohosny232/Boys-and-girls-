404042a1ed93dfb12bf9b425057e890fdb3746b4
Mohosny232 <mohosny1114@gmail.com>
2026-09-22 18:45:14 +0300

    Upgrade service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,16 +1,28 @@
-const CACHE_NAME = "app-cache-v5";
+const CACHE_NAME = "app-cache-v6";
 
 self.addEventListener("install", event => {
     self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./manifest.json"
+            ]);
+        })
+    );
 });
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.map(key => caches.delete(key))
-            )
-        ).then(() => self.clients.claim())
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
@@ -18,8 +30,12 @@ self.addEventListener("fetch", event => {
     if (event.request.method !== "GET") return;
 
     event.respondWith(
-        fetch(event.request).catch(() =>
-            caches.match(event.request)
-        )
+        fetch(event.request)
+            .then(response => {
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
 });
